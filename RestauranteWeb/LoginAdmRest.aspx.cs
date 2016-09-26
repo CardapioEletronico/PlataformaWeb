657a5252bde814ff100ddea2230562db6614c410
Gilson Gabriel Martins Diniz <lhyngrha@hotmail.com>
2016-09-26 10:42:48 -0300

    Senha do usuário Protegida

--- RestauranteWeb/LoginAdmRest.aspx.cs
+++ RestauranteWeb/LoginAdmRest.aspx.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -28,10 +29,9 @@ namespace RestauranteWeb.AdmRest
             var str = response.Content.ReadAsStringAsync().Result;
             List<Models.AdminRest> obj = JsonConvert.DeserializeObject<List<Models.AdminRest>>(str);
 
-
             foreach (Models.AdminRest x in obj)
             {
-                if(x.Usuario == TextBoxUsuario.Text && x.Senha == TextBoxSenha.Text)
+                if(x.Usuario == TextBoxUsuario.Text && ValidatePassword(TextBoxSenha.Text, x.Senha))
                 {
                     Session["Login"] = TextBoxUsuario.Text;
                     Session["s"] = TextBoxSenha.Text;
@@ -41,6 +41,43 @@ namespace RestauranteWeb.AdmRest
                 }
             }
             Response.Write("<script>window.alert('Usuário não encontrado'); </script>");
+        }
+
+
+        public const int SaltByteSize = 24;
+        public const int HashByteSize = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash 
+        public const int Pbkdf2Iterations = 1000;
+        public const int IterationIndex = 0;
+        public const int SaltIndex = 1;
+        public const int Pbkdf2Index = 2;
+
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+            char[] delimiter = { ':' };
+            var split = correctHash.Split(delimiter);
+            var iterations = Int32.Parse(split[IterationIndex]);
+            var salt = Convert.FromBase64String(split[SaltIndex]);
+            var hash = Convert.FromBase64String(split[Pbkdf2Index]);
+
+            var testHash = GetPbkdf2Bytes(password, salt, iterations, hash.Length);
+            return SlowEquals(hash, testHash);
+        }
+
+        private static bool SlowEquals(byte[] a, byte[] b)
+        {
+            var diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+            {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+            return diff == 0;
+        }
+
+        private static byte[] GetPbkdf2Bytes(string password, byte[] salt, int iterations, int outputBytes)
+        {
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            pbkdf2.IterationCount = iterations;
+            return pbkdf2.GetBytes(outputBytes);
         }
     }
 }
--- RestauranteWeb/WebCRUDAdminRest.aspx.cs
+++ RestauranteWeb/WebCRUDAdminRest.aspx.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Web;
 using System.Web.UI;
@@ -77,15 +78,43 @@ namespace RestauranteWeb
             Reload();
         }
 
-        protected async void btnInsert_Click(object sender, EventArgs e)
+        public const int SaltByteSize = 24;
+        public const int HashByteSize = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash 
+        public const int Pbkdf2Iterations = 1000;
+        public const int IterationIndex = 0;
+        public const int SaltIndex = 1;
+        public const int Pbkdf2Index = 2;
+
+        public static string HashPassword(string password)
+        {
+            var cryptoProvider = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[SaltByteSize];
+            cryptoProvider.GetBytes(salt);
+
+            var hash = GetPbkdf2Bytes(password, salt, Pbkdf2Iterations, HashByteSize);
+            return Pbkdf2Iterations + ":" + Convert.ToBase64String(salt) + ":" + Convert.ToBase64String(hash);
+            //return Convert.ToBase64String(hash);
+        }
+
+        private static byte[] GetPbkdf2Bytes(string password, byte[] salt, int iterations, int outputBytes)
+        {
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            pbkdf2.IterationCount = iterations;
+            return pbkdf2.GetBytes(outputBytes);
+        }
+
+
+    protected async void btnInsert_Click(object sender, EventArgs e)
         {
             HttpClient httpClient = new HttpClient();
+
+            string cu = HashPassword(textBoxSenha.Text);
 
             httpClient.BaseAddress = new Uri(ip);
             Models.AdminRest f = new Models.AdminRest
             {
                 Usuario = textBoxUsuario.Text,
-                Senha = textBoxSenha.Text,
+                Senha = cu,
                 Restaurante_id = int.Parse(Restaurantes.SelectedItem.Value)
             };
 
